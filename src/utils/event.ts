3d69f2d447546c644fde918d5e81aae3afd89333
KCFindstr <792491320@qq.com>
2024-04-05 00:37:08 -0700

    Move event to @rurino/core

---
+++ src/utils/event.ts
@@ -0,0 +1,15 @@
+export class MeguEvent<T = void> {
+  private handlers: ((data: T) => void)[] = [];
+
+  public add(handler: (data: T) => void) {
+    this.handlers.push(handler);
+  }
+
+  public remove(handler: (data: T) => void) {
+    this.handlers = this.handlers.filter((h) => h !== handler);
+  }
+
+  public fire(data: T) {
+    this.handlers.forEach((h) => h(data));
+  }
+}
