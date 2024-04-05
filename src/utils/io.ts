0d5f554c98a1b75f5bdb5e45356fecc413b865d6
KCFindstr <792491320@qq.com>
2024-04-05 00:57:12 -0700

    Move IO to @rurino/core

---
+++ src/utils/io.ts
@@ -0,0 +1,49 @@
+import YAML from 'yaml';
+export type LookupFunc<T> = (key: string) => T | undefined;
+export type Finalizer<T> = (f: LookupFunc<T>) => void;
+
+export interface ISerializable {
+  serialize(): any;
+}
+
+export interface IFinalizable<T> {
+  deserialize(data: any): Finalizer<T>;
+}
+
+export function SerializeYaml(data: ISerializable): string {
+  return YAML.stringify(data.serialize());
+}
+
+export function DeserializeYaml(data: string): any {
+  return YAML.parse(data);
+}
+
+export function Gzip(data: ISerializable): Promise<Blob> {
+  const ds = new CompressionStream('gzip');
+  const blob = new Blob([JSON.stringify(data.serialize())]);
+  let decompressedStream = blob.stream().pipeThrough(ds);
+  return new Response(decompressedStream).blob();
+}
+
+export function PathJoin(...parts: string[]): string {
+  parts = parts.map((part) => part.replaceAll('\\', '/').replace(/\/+$/g, ''));
+  return parts.join('/');
+}
+
+export function PathBaseDir(path: string): string {
+  const parts = path.replaceAll('\\', '/').split('/');
+  return PathJoin(...parts.slice(0, -1));
+}
+
+export function CmpVersion(v1: string, v2: string): number {
+  const v1Parts = v1.split('.').map((p) => parseInt(p));
+  const v2Parts = v2.split('.').map((p) => parseInt(p));
+  const len = Math.max(v1Parts.length, v2Parts.length);
+  for (let i = 0; i < len; i++) {
+    const p1 = v1Parts[i] ?? 0;
+    const p2 = v2Parts[i] ?? 0;
+    if (p1 < p2) return -1;
+    if (p1 > p2) return 1;
+  }
+  return 0;
+}
